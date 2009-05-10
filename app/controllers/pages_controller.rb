09af7b9e38f52a623ba1d3a9943d9f9bc26d3597
jhannes <johannes@brodwall.com>
2009-05-10 14:08:54 +0000

    Turned on page caching, SVN ignore cached pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@
 # See http://railscasts.com/episodes/117-semi-static-pages for details
 class PagesController < ApplicationController
 
-	# Generic controller for serving static content
-	# See routes.rb for specification of existing pages
-	# See views/pages for the pages themselfe
+  # Page cache for all views.
+  # See http://www.railsenvy.com/2007/2/28/rails-caching-tutorial for details
+  caches_page :index, :arrangoerene, :lyntaler, :openspace
 	
 end
 
