2bf1877f2787587dc9e883e56fbe6b3004d3d0d5
smidig <dev@smidig.no>
2009-06-01 21:54:15 +0000

    Added registration with payment

---
+++ db/migrate/20090601185448_add_dinner_and_early_bird_to_registrations.rb
@@ -0,0 +1,13 @@
+class AddDinnerAndEarlyBirdToRegistrations < ActiveRecord::Migration
+  def self.up
+    add_column :registrations, :is_earlybird, :boolean
+    add_column :registrations, :includes_dinner, :boolean
+    add_column :registrations, :description, :string
+  end
+
+  def self.down
+    remove_column :registrations, :is_earlybird
+    remove_column :registrations, :includes_dinner
+    remove_column :registrations, :description
+  end
+end
