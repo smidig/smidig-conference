1d2de94b215c0d89120d9c9ed72ecbe942a0f2b6
smidig <dev@smidig.no>
2009-06-10 19:20:24 +0000

    For-norsket siden og bygget en støttefunksjon for riktig ingen-, entalls- og flertallsform av lyntaler

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -4,4 +4,12 @@ module TopicsHelper
     @topic_offset = topic_offset
     render :partial => 'topics/program' if @topic
   end
+
+  def antall_lyntaler(antall)
+    case antall
+      when 0 then return "Ingen lyntaler"
+      when 1 then return "1 lyntale"
+      else return "#{antall} lyntaler"
+    end
+  end
 end
