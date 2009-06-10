cb7ed097c3e69dcd81c5b85f5ca346f3cdeacbf9
smidig <dev@smidig.no>
2009-06-10 19:44:25 +0000

    For-norsket topic og la til en helpermetode for antall stemmer

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,2 +1,9 @@
 module TalksHelper
+  def antall_stemmer(antall)
+    case antall 
+      when 0 then return "Ingen stemmer"
+      when 1 then return "1 stemme"
+      else return "#{antall} stemmer"
+    end
+  end
 end
