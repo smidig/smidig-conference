cb3473c108c6e5cc361737cd56a50955b6d71e0a
oc <oc@rynning.no>
2009-06-24 17:16:56 +0000

    Fixed failing test

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
   
-  def test_should_login_and_logout    
+  def test_should_block_restricted_pages_when_not_logged_in
     get :restricted
     assert !UserSession.find    
     
@@ -10,9 +10,14 @@ class UserSessionsControllerTest < ActionController::TestCase
     
     get :restricted
     assert UserSession.find
-    
+  end
+  
+  def test_should_login_and_logout
+    login_as :quentin
+    assert UserSession.find
     get :logout
     assert !UserSession.find
   end
+  
     
 end
