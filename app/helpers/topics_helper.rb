0c4f093967a4f0be7d6552b99116e0cf60592f7f
oc <oc@rynning.no>
2009-07-24 17:03:51 +0000

    Improved text for "additional lightning talks"

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -16,8 +16,8 @@ module TopicsHelper
   def antall_flere_lyntaler(antall_flere)
     case antall_flere
       when -100..0 then return ""
-      when 1 then return "1 lyntale til foreslått"
-      else return "#{antall_flere} flere lyntaler foreslått"
+      when 1 then return "... og en lyntale til"
+      else return "... og #{antall_flere} flere lyntaler"
     end    
   end
 end
