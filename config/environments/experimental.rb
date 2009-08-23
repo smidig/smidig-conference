a407da17c3d59c3f143743aacdd635392ff96e93
jhannes <johannes@brodwall.com>
2009-08-23 23:42:53 +0000

    Don't try to deliver emails in development, experimental or staging environments

--- config/environments/experimental.rb
+++ config/environments/experimental.rb
@@ -14,4 +14,5 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = true
 
 # Don't care if the mailer can't send
+config.action_mailer.perform_deliveries = false
 config.action_mailer.raise_delivery_errors = false
