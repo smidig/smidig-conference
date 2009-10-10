ec6d734a27fcb594b94d0bd3c4764b0003b89113
jhannes <johannes@brodwall.com>
2009-10-10 01:12:54 +0000

    Reduce production logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,3 +22,5 @@ config.action_view.cache_template_loading            = true
 # config.action_mailer.raise_delivery_errors = false
 
 INCLUDE_UNFINISHED = false
+
+Rails::logger.level = Logger::INFO
