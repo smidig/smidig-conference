8021cd20def41004496185b0166f78b9ad3cc773
Ole Christian Rynning <oc@rynning.no>
2010-05-13 20:51:06 +0200

    Changed to Rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ tmp_environment = File.join(File.dirname(__FILE__), '../tmp/environment.rb')
 File.exist?(tmp_environment) and load(tmp_environment)
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
