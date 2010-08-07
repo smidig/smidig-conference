e8b5530f5714ea5dfed726b62b0ce4ac5b129e4e
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2010-08-07 13:10:52 +0200

    using bundler! tweaks added (config/boot.rb and config/preinitializer.rb) to make it work for rails 2.3.x. Removed some rubbish files (oc.rb and glassfishXX)

---
+++ Gemfile
@@ -0,0 +1,14 @@
+#Kjør ruby versjon 1.8.7 !
+#gem install bundler
+
+source :gemcutter
+gem 'rails', '= 2.3.8'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+#gem 'authlogic', '2.0.11'
+gem 'gchartrb'
+
+group :plugins do
+  gem 'authlogic', '2.0.11'
+end
+gem 'faker', :group => 'development'
+gem 'populator', :group => 'development'
--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ tmp_environment = File.join(File.dirname(__FILE__), '../tmp/environment.rb')
 File.exist?(tmp_environment) and load(tmp_environment)
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -26,18 +26,13 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  
-  config.gem "authlogic", :version => "2.0.11"
-  
-  config.gem "gchartrb", :lib => "google_chart"
-  
+
   #config.gem "authlogic-oid", :lib => "authlogic_openid"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  config.plugins = [:all]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
