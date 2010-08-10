1fcfe4152b531facb91954512fe7526b8fdb1592
Sarah Brodwall <sjbrodwall@gmail.com>
2010-08-10 02:14:02 +0200

    Changed the name "promotional email" to something that wouldn't be adblocked.

---
+++ db/migrate/20100810000917_rename_email_acceptance_field.rb
@@ -0,0 +1,11 @@
+class RenameEmailAcceptanceField < ActiveRecord::Migration
+  def self.up
+    add_column :users, :accept_optional_email, :boolean
+    remove_column :users, :accept_promotional_email
+  end
+
+  def self.down
+    add_column :users, :accept_promotional_email, :boolean
+    remove_column :users, :accept_optional_email, :boolean
+  end
+end
