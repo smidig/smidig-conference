e827956b7df88591e54c128861ab135c5271ed00
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2010-08-18 13:36:25 +0200

    saving payment notification with object ref rather than id

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,15 +1,15 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
   def create
-    PaymentNotification.create!(:params => params, :registration_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id], :paid_amount => params[:mc_gross], :currency => params[:mc_currency])
-    
     registration = Registration.find_by_invoice(params[:invoice])
     registration.payment_notification_params = params
     registration.paid_amount = params[:mc_gross].to_i
     registration.payment_reference = params[:txn_id]
     registration.payment_complete_at = Time.now
     registration.registration_complete = (registration.paid_amount == registration.price)
-    registration.save
+    registration.save!
+    
+    PaymentNotification.create!(:params => params, :registration => registration, :status => params[:payment_status], :transaction_id => params[:txn_id], :paid_amount => params[:mc_gross], :currency => params[:mc_currency])
     
     SmidigMailer.deliver_payment_confirmation(registration)
     
