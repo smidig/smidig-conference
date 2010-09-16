f09b482618ce52f0c330d94fce6c870ff1645c1a
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2010-09-16 21:27:38 +0200

    config.gem 'somegem' in config/ is replaced by bundler Gemfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,11 +16,3 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.perform_deliveries = false
 config.action_mailer.raise_delivery_errors = false
-
-
-config.gem "faker"
-config.gem "populator"
-#config.gem "sqlite3-ruby", :version => "1.2.3"
-#config.gem "net-ssh"
-#config.gem "net-scp"
-
