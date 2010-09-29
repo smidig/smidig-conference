cc998cd6e2da5e9ee21a4edd60ba387ec8de3290
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2010-09-30 00:23:02 +0200

    added output to migration

--- db/migrate/20100929194420_add_comments_counter_to_talk.rb
+++ db/migrate/20100929194420_add_comments_counter_to_talk.rb
@@ -4,6 +4,7 @@ class AddCommentsCounterToTalk < ActiveRecord::Migration
 
     Talk.all.each do |talk|
       talk.comments_count = talk.comments.size
+      puts "adding comment count #{talk.comments_count} to #{talk.title}"
       talk.save!
     end
   end
