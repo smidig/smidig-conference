019ca0bbe8fc45e0ca8f07e85000a31195bb2323
Ivar Conradi Østhus <ivarconr@gmail.com>
2010-10-09 16:15:54 +0200

    added cache-control header for static resources

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,6 +1,11 @@
 # Controller for displaying static html pages
 # See http://railscasts.com/episodes/117-semi-static-pages for details
 class InfoController < ApplicationController
+  before_filter :applyCacheControl
+
+  def applyCacheControl
+    response.headers['Cache-Control'] = 'public, max-age=3600'
+  end
 
   # Page cache for all views.
   # See http://www.railsenvy.com/2007/2/28/rails-caching-tutorial for details
