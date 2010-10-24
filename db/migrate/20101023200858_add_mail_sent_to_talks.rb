26ff200841fa8bff066fd69adf7ba1b261fb4d45
Karianne Berg <karianne.berg@objectware.no>
2010-10-25 00:23:49 +0200

    how 'bout now?

--- db/migrate/20101023200858_add_mail_sent_to_talks.rb
+++ db/migrate/20101023200858_add_mail_sent_to_talks.rb
@@ -1,9 +1,9 @@
 class AddMailSentToTalks < ActiveRecord::Migration
   def self.up
-    add_column :talks, :email_sent, :boolean, :default => 0
+    add_column :talks, :email_sent, :boolean, :default => false
     Talk.all.each do |talk|
       if talk.acceptance_status == "accepted" || talk.acceptance_status == "refused" 
-        talk.email_sent = 1
+        talk.email_sent = true
         talk.save
       end
     end
