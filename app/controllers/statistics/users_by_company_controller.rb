9e4fe39b392c3e75acebfcb31ca1856273a5adb6
Karianne Berg <karianne.berg@objectware.no>
2010-10-26 23:33:29 +0200

    hallo igjen?

--- app/controllers/statistics/users_by_company_controller.rb
+++ app/controllers/statistics/users_by_company_controller.rb
@@ -7,10 +7,10 @@ class Statistics::UsersByCompanyController < ApplicationController
       case params[:filter]
         when 'speakers'
           @title = 'Antall deltakere med innsendt foredrag pr selskap'
-          @companies = User.find_by_sql("SELECT company, count(*) AS num_of_participants FROM (SELECT DISTINCT u.name, u.company FROM users u INNER JOIN speakers s ON u.id = s.user_id INNER JOIN talks t ON t.id = s.talk_id ORDER BY u.company, u.name) GROUP BY company ORDER BY num_of_participants DESC, company")
+          @companies = User.find_by_sql("SELECT company, count(*) AS num_of_participants FROM (SELECT DISTINCT u.name, u.company FROM users u INNER JOIN speakers s ON u.id = s.user_id INNER JOIN talks t ON t.id = s.talk_id ORDER BY u.company, u.name) AS s GROUP BY company ORDER BY num_of_participants DESC, company")
         when 'accepted'
           @title = 'Antall deltakere med godkjent foredrag pr selskap'
-          @companies = User.find_by_sql("SELECT company, count(*) AS num_of_participants FROM (SELECT DISTINCT u.name, u.company FROM users u INNER JOIN speakers s ON u.id = s.user_id INNER JOIN talks t ON t.id = s.talk_id WHERE t.acceptance_status = 'accepted' ORDER BY u.company, u.name) GROUP BY company ORDER BY num_of_participants DESC, company")
+          @companies = User.find_by_sql("SELECT company, count(*) AS num_of_participants FROM (SELECT DISTINCT u.name, u.company FROM users u INNER JOIN speakers s ON u.id = s.user_id INNER JOIN talks t ON t.id = s.talk_id WHERE t.acceptance_status = 'accepted' ORDER BY u.company, u.name) AS s GROUP BY company ORDER BY num_of_participants DESC, company")
         else
           flash[:error] = "Ukjent filter"
           @title = ""
