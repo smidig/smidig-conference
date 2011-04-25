898644b0af23d42cd04ef7ba0a5a753e6f0bfbc7
Johannes Brodwall <johannes@brodwall.com>
2011-04-25 22:46:20 +0200

    Overwrote with Rails 3 files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,32 +1,26 @@
-# Settings specified here will take precedence over those in config/environment.rb
+SmidigConference::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# In the development environment your application's code is reloaded on
-# every request.  This slows down response time but is perfect for development
-# since you don't have to restart the webserver when you make code changes.
-config.cache_classes = false
+  # In the development environment your application's code is reloaded on
+  # every request.  This slows down response time but is perfect for development
+  # since you don't have to restart the webserver when you make code changes.
+  config.cache_classes = false
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = false
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
+  config.action_controller.perform_caching = false
 
-# Don't care if the mailer can't send
-config.action_mailer.perform_deliveries = false
-config.action_mailer.raise_delivery_errors = false
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
 
-skip=true or config.after_initialize do
-  Bullet.enable = true
-  Bullet.alert = false
-  Bullet.bullet_logger = true
-  Bullet.console = false
-  Bullet.growl = false
-#  Bullet.xmpp = { :account => 'bullets_account@jabber.org',
-#                  :password => 'bullets_password_for_jabber',
-#                  :receiver => 'your_account@jabber.org',
-#                  :show_online_status => true }
-  Bullet.rails_logger = true
-  Bullet.disable_browser_cache = true
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
 end
+
