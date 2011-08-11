26258e0fb16d4a3d4fe1f45f5ee69f93770f6867
Ivar Nilsen <ivar.nilsen@gmail.com>
2011-08-11 21:50:37 +0200

    Added two new talktypes, edited infotexts on talk registration form and fixed some css borders on edit user and edit talk views

---
+++ db/migrate/20110811193211_add_new_workshop_types.rb
@@ -0,0 +1,15 @@
+class AddNewWorkshopTypes < ActiveRecord::Migration
+  def self.up
+    TalkType.where(:name => "Workshop").first.destroy
+
+    TalkType.new(:name => "Kort workshop", :duration => "45 minutter").save!
+    TalkType.new(:name => "Lang workshop", :duration => "90 minutter").save!
+  end
+
+  def self.down
+    TalkType.where(:name => "Lang workshop").first.destroy
+    TalkType.where(:name => "Kort workshop").first.destroy
+
+    TalkType.new(:name => "Workshop", :duration => "90 minutter").save!
+  end
+end
