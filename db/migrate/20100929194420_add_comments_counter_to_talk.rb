4439527112ae1f9c6d616fdc765f6fe1de912dae
Johannes Brodwall <johannes@brodwall.com>
2011-08-11 22:53:39 +0200

    Removed data migration which didn't work on empty db

--- db/migrate/20100929194420_add_comments_counter_to_talk.rb
+++ db/migrate/20100929194420_add_comments_counter_to_talk.rb
@@ -1,15 +1,6 @@
 class AddCommentsCounterToTalk < ActiveRecord::Migration
   def self.up
     add_column :talks, :comments_count, :integer
-
-    Talk.all.each do |talk|
-      Talk.update_counters(talk.id, :comments_count =>talk.comments.size)
-    end
-
-    #Visual confirmation
-    Talk.all.each do |talk|
-      puts "added comment count #{talk.comments_count} to #{talk.title}"
-    end
   end
 
   def self.down
