deea708c1f490916b6e0535e9a8bdab710951ee7
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2011-08-27 14:50:25 +0200

    tabs to space

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < ApplicationController
   before_filter :require_user
   before_filter :require_admin_or_owner, :except => [:index]
   before_filter :require_admin, :only => [:index, :delete, :confirm_delete, :phone_list]
-  
+
   def index
     @registrations = Registration.find_by_params(params)
     @ticket_types = @registrations.collect { |r| r.ticket_type }.uniq
@@ -13,8 +13,8 @@ class RegistrationsController < ApplicationController
     @date_range = (first_registration.created_at.to_date-1..Date.today).to_a
     @all_per_date = total_by_date(@registrations, @date_range)
     @registrations_per_ticket_type_per_date = per_ticket_type_by_date(@registrations, @date_range)
-    @paid_per_date = total_by_date(@registrations, @date_range)    
-    
+    @paid_per_date = total_by_date(@registrations, @date_range)
+
     @income_per_date = total_price_per_date(@registrations, @date_range)
   end
 
@@ -26,15 +26,15 @@ class RegistrationsController < ApplicationController
     @date_range = (first_registration.created_at.to_date-1..Date.today).to_a
     @all_per_date = total_by_date(@registrations, @date_range)
     @registrations_per_ticket_type_per_date = per_ticket_type_by_date(@registrations, @date_range)
-    @paid_per_date = total_by_date(@registrations, @date_range)    
-    
+    @paid_per_date = total_by_date(@registrations, @date_range)
+
     @income_per_date = total_price_per_date(@registrations, @date_range)
   end
-	
-  
+
+
   def edit
-  	@registration = Registration.find(params[:id])
-  	@registration.payment_notification ||= PaymentNotification.new
+    @registration = Registration.find(params[:id])
+    @registration.payment_notification ||= PaymentNotification.new
   end
 
   # PUT /registrations/1
@@ -52,7 +52,7 @@ class RegistrationsController < ApplicationController
         @registration.payment_reference = params[:registration][:payment_reference]
         @registration.paid_amount = params[:registration][:paid_amount]
         @registration.invoiced = params[:registration][:invoiced]
-        @registration.user.is_admin = 
+        @registration.user.is_admin =
           (@registration.ticket_type == "organizer" && @registration.registration_complete)
         @registration.user.save!
       end
@@ -60,21 +60,21 @@ class RegistrationsController < ApplicationController
 
     if @registration.update_attributes(params[:registration])
       flash[:notice] = "Informasjonen er oppdatert#{' - husk å sende mail til bruker' if admin?}"
-	    redirect_to @registration.user
-	  else
+      redirect_to @registration.user
+    else
       flash.now[:error] = 'Kunne ikke oppdatere informasjonen'
       render :action => "edit"
-	  end
+    end
   end
-  
+
   def delete
     @registration = Registration.find(params[:id])
-    
+
     if params[:name][0..2].downcase == params[:confirmation][0..2].downcase
       @registration.user.talks.delete
       @registration.user.delete
       @registration.delete
-    
+
       flash[:notice] = "Slettet bruker #{@registration.user.name}"
       redirect_to :action => 'index'
     else
@@ -98,7 +98,7 @@ protected
       store_location
       flash[:notice] = "Du må være logget inn for å se siden."
       redirect_to new_user_session_url
-      return false      
+      return false
     end
     @registration = Registration.find(params[:id]) unless params[:id].blank?
     if (@registration && @registration.user == current_user) || personal_admin?
