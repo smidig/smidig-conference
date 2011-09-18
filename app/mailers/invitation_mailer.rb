b08f6444f40a615ed1839db15fb61aa2353c8e8c
Ivar Nilsen <ivar.nilsen@gmail.com>
2011-09-18 17:36:44 +0200

    One-time script for sending invitation emails

---
+++ app/mailers/invitation_mailer.rb
@@ -0,0 +1,10 @@
+class InvitationMailer < ActionMailer::Base
+
+  def invitation_mail(email)
+    recipients "<#{email}>"
+    from       "Smidig konferansen"
+    subject    "Vil du være med på Smidig 2011?"
+    sent_on    Time.now
+  end
+
+end
