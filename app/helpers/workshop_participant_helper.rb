ed8c6d0b70e4e76d2a1d8588ed0bf70e5fedaa1f
Kris <kris@wltrs.org>
2011-11-09 21:53:10 +0100

    Added link from workshop to participants

--- app/helpers/workshop_participant_helper.rb
+++ app/helpers/workshop_participant_helper.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 module WorkshopParticipantHelper
   def workshop_participation_link(talk, user)
     if talk.participant?(user)
@@ -12,6 +13,16 @@ module WorkshopParticipantHelper
       """
       <span class='join-workshop'>Sign up!</span>
       """.html_safe
+    end
+  end
+
+  def conditional_link_to_participant_list(talk)
+    n = talk.participants.count
+    if (n == 0)
+      "Ingen deltagere har registrert seg ennå."
+    else
+      link_to pluralize(n, 'deltaker', 'deltakere'),
+            talk_workshop_participant_index_path(talk)
     end
   end
 end
