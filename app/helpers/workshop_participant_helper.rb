f676e85245c34c50fc86f81227af58032686d421
Kris <kris@wltrs.org>
2011-11-09 23:25:07 +0100

    Added workshop registration links

--- app/helpers/workshop_participant_helper.rb
+++ app/helpers/workshop_participant_helper.rb
@@ -2,17 +2,16 @@
 module WorkshopParticipantHelper
   def workshop_participation_link(talk, user)
     if talk.participant?(user)
-      """
-      <span class='leave-workshop'>Leave</span>
-      """.html_safe
+      wsp = talk.workshop_participants.where(:user_id => user.id).first
+      button_to 'Leave', talk_workshop_participant_path(talk, wsp),
+              :confirm => "Sure?", :method => :delete, :class => 'leave-workshop'
     elsif talk.complete?
       """
       <span class='workshop-full'>Sorry, full</span>
       """.html_safe
     else
-      """
-      <span class='join-workshop'>Sign up!</span>
-      """.html_safe
+      button_to 'Sign up!', talk_workshop_participant_index_path(talk),
+              :method => :post, :class => 'join-workshop'
     end
   end
 
